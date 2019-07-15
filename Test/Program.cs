a55c42eed3d64bc93db0b0443b38678da861521d
ikorin24 <sunshinexxab@yahoo.co.jp>
2019-07-16 01:04:36 +0900

    Add TestProject

---
+++ Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fbx;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new FbxParser();
+            var cube = parser.Parse("../../cube.fbx");
+            Debug.WriteLine(cube.DumpJson());
+
+            //using (var writer = new StreamWriter("cube.json"))
+            //{
+            //    writer.WriteLine(cube.DumpJson());
+            //}
+            var dice = parser.Parse("../../Dice.fbx");
+            Debug.WriteLine(dice.DumpJson());
+            //using (var writer = new StreamWriter("Dice.json"))
+            //{
+            //    writer.WriteLine(dice.DumpJson());
+            //}
+
+
+            Console.WriteLine("Press Any Key...");
+            Console.ReadKey();
+        }
+    }
+}
